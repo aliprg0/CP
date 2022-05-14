55f539d4ecf59a2f5ccb92c731823b9c479457bc
aliprg0 <aliprg0@gmail.com>
2022-05-14 17:36:17 +0430

    predict method v3 - added models selection

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ from tensorflow.keras.models import load_model
 import os
 from datetime import datetime
 
-def load_models():
+def load_models(timeframe):
     global models
     models = {}
-    for file in os.listdir("models"):
-        if file.endswith(".h5"):
-            models[file.split(".")[0]] = load_model(f"models/{file}")
+    if timeframe == "all":
+        for root, dirs, files in os.walk("./models"):
+            for file in files:
+                if file.endswith(".h5"):
+                    models[file] = load_model(os.path.join(root, file))
+    else:
+        for root, dirs, files in os.walk(f"./models/{timeframe}"):
+            for file in files:
+                if file.endswith(".h5"):
+                    models[file] = load_model(os.path.join(root, file))
     
-
 def scaler(row):
     scaler = MinMaxScaler(feature_range=(-1, 1))
     row = scaler.fit_transform(row)
     return row
-
 
 def process_for_prediction(data, index):
         i = index
@@ -30,7 +35,6 @@ def process_for_prediction(data, index):
               data.drop("datetime", axis=1, inplace=True)
         if "Adj Close" in data.columns:
               data.drop("Adj Close", axis=1, inplace=True)
-
         data = np.array(data)
         grow = []
         ggggrow = []
@@ -60,7 +64,6 @@ def make_prediction_for_tv(symbol, exchange, timeframe,tindex):
       tresults.append(
           f"{models_reverse.get(model)} : {prediction}")
       t_only_num.append(prediction)
-
 
 def make_prediction(ysymbol, period, timeframe, tsymbol, texchange, ttimeframe, tindex, index, tsymbol2):
     make_prediction_for_yf(ysymbol, period, timeframe,index)
@@ -100,7 +103,6 @@ def yf_data_handler(symbol, period, timeframe):
     data = yf.download(symbol, period=period, interval=timeframe)
     index_c = data.index.values
     index = index_c[-3:]
-    
     if timeframe == "15m":
         mins = []
         for i in index:
@@ -114,54 +116,45 @@ def yf_data_handler(symbol, period, timeframe):
             return -2,index
         else :
             return -1,index
-      
     if timeframe == "1d":
        return -1,index
-    
     if timeframe == "1wk":
         print(str(index[-2]).strip().split("-")[-1][:2])
         if int(str(index[-2]).strip().split("-")[-1][:2]) - int(str(index[-1]).strip().split("-")[-1][:2]) != 7:
             return -2,index
         else:
             return -1,index
-
     if timeframe == "1mo":
         if int(str(index[-2]).split("-")[-2]) == int(str(index[-1]).split("-")[-2]):
             return -2,index
         else:
             return -1,index
 
-      
-def predict(symbol,timeframe,future_prediction=False):
+def predict(symbol,timeframe,future_prediction=False,using_all_models=False):
     global yresults
     global tresults
     global models_reverse
     global y_only_num
     global t_only_num
-
-    load_models()
+    if using_all_models:
+        load_models("all")
+    else:
+        load_models(timeframe)
     yresults = []
     tresults = []
     y_only_num = []
     t_only_num = []
-    
     exchange = "binance"
-
     models_reverse = {v: k for k, v in models.items()}
     ttimeframe = interval_handler(timeframe)
     period = period_handler(timeframe)
     ysymbol,tsymbol,tsymbol2 = symbol_handler(symbol)
-
-    
     tindex = -1
     index,last_three = yf_data_handler(ysymbol, period, timeframe)
-    
     if future_prediction:
         tindex = 0
         index = index + 1
-
     make_prediction(ysymbol, period, timeframe,tsymbol,exchange,ttimeframe,tindex,index,tsymbol2)
-
     y_buy = 0
     y_sell = 0
     for i in y_only_num:
@@ -169,7 +162,6 @@ def predict(symbol,timeframe,future_prediction=False):
           y_buy += 1
        else:
             y_sell += 1
-
     t_buy = 0
     t_sell = 0
     for i in t_only_num:
@@ -177,17 +169,14 @@ def predict(symbol,timeframe,future_prediction=False):
           t_buy += 1
        else:
            t_sell += 1
-
     all_buy = y_buy + t_buy
     all_sell = y_sell + t_sell
     plus = all_buy - all_sell
-
-
     if y_buy > y_sell:
         suggestion = "BUY"
     else:
         suggestion = "SELL"
-    
+
     info = f'''
         YF index: {index}
         TV index: {tindex}
@@ -207,10 +196,8 @@ def predict(symbol,timeframe,future_prediction=False):
         Sells: {all_sell}
         All: {plus}
         Suggestion: {suggestion}'''
-
     return info
 
-
-info = predict("btc", "1h", future_prediction=False)
+info = predict("btc", "1mo", future_prediction=True, using_all_models=True)
 print(info)
     
